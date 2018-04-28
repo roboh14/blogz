06d39be36e4e83b1ae6d84848d8d98c30de5fd94
Robert Holloway <robertcar@sbcglobal.net>
2018-04-28 13:55:56 -0500

    blogz complete

--- main.py
+++ main.py
@@ -6,38 +6,112 @@ from datetime import datetime
 # SQL Alchemy Configuration --Standard
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 # Needed to secure application and database
 app.secret_key = 'y337kGcys&zP3B'
 
 # First Table (3 Columns - id, title, post)
-class Task(db.Model):
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # Numerical id list
-    btitle = db.Column(db.String(120))            # Title
-    entry = db.Column(db.String(240))             # Blog post contained here
+    title = db.Column(db.String(120))            # Title
+    body = db.Column(db.String(240))             # Blog post contained here
     pub_date = db.Column(db.DateTime)             # Time stamp
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
     # Initilize Function - No clue what it does, but you need it
-    def __init__(self, btitle, entry, pub_date = None):
-        self.btitle = btitle
-        self.entry = entry
+    def __init__(self, title, body, owner, pub_date = None):
+        self.title = title
+        self.body = body
+        self.owner = owner
         if pub_date is None:
             pub_date = datetime.utcnow()
         self.pub_date = pub_date
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120), unique = True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref = 'owner')
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'blog', 'index', 'static']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = User.query.filter_by(username=username)
+        if users.count() == 1:
+            user = users.first()
+            if password == user.password:
+                session['username'] = user.username
+                flash('welcome back, '+user.username)
+                return redirect("/")
+        flash('bad username or password')
+        return redirect("/login")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        username_db_count = User.query.filter_by(username=username).count()
+        if username_db_count > 0:
+            flash('yikes! "' + username + '" is already taken and password reminders are not implemented')
+            return redirect('/signup')
+        if password != verify:
+            flash('passwords did not match')
+            return redirect('/signup')
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        session['username'] = user.username
+        return redirect("/")
+    else:
+        return render_template('signup.html')
 
 # Query to return full list of entries by Pub date
 def getall():
     return Task.query.order_by(Task.pub_date.desc())
 
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    blog_id = request.args.get('id')
+    user_id = request.args.get('userid')
+    # posts = Blog.query.all()
+    # Recent blog posts order to top.
+    posts = Blog.query.order_by(Blog.pub_date.desc())
+
+    if blog_id:
+        post = Blog.query.filter_by(id=blog_id).first()
+        return render_template("post.html", title=post.title, body=post.body, user=post.owner.username, pub_date=post.pub_date, user_id=post.owner_id)
+    if user_id:
+        entries = Blog.query.filter_by(owner_id=user_id).all()
+        return render_template('singleUser.html', entries=entries)
+
+    return render_template('blog.html', posts=posts)
+
 
 # Main App Route
 @app.route('/', methods=['POST','GET'])     # Both methods needed, to filter requests
 def index():
-    return render_template('entry.html', title = "Build-a-Blog!", allblogs = getall())  # This populates main blog page
-
-
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 # New Post App Route
 @app.route('/new-post', methods=['POST','GET'])     # Both methods needed, to filter requests
@@ -59,7 +133,8 @@ def new_post():
             task_name = ''
 
         if not title_error and not post_error:              # If no errors are present
-            new_post = Task(blog_title, task_name)          # Sets up columns with values
+            owner = User.query.filter_by(username = session['username']).first()
+            new_post = Blog(blog_title, task_name, owner)          # Sets up columns with values
             db.session.add(new_post)                        # Adds title, entry to database
             db.session.commit()                             # Commits all session changes
             new_id = new_post.id                            # Collects newly added post Id
@@ -67,14 +142,11 @@ def new_post():
  
     return render_template('new-post.html', title_error = title_error, post_error= post_error, blog_title = blog_title, task_name = task_name)
 
-# App Route for Individual Posts
-@app.route('/blog', methods=['POST','GET'])     # Both methods needed, to filter requests
-def single():
-    blog_id = request.args.get('id')                                            # Collects id tag
-    post = Task.query.filter_by(id=blog_id).first()                             # Filters Table by id
-    return render_template("blog.html", btitle=post.btitle, entry=post.entry, pub_date = post.pub_date)   # Renders template based on individual id (Use case 1)
 
-
+@app.route("/logout")
+def logout():
+    del session['username']
+    return redirect("/login")
 
 # I need to review what this really does....lol
 if __name__ == '__main__':
